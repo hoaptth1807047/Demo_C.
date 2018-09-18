2c62ea8f7fb2ef47a81a5c5168b29c0cda3b10db
Pham Thanh Hoa <hoaptth1807047@fpt.edu.vn>
2018-09-18 17:53:09 +0700

    Code bay gio moi du qua bai thi.

--- main.c
+++ main.c
@@ -2,8 +2,19 @@
 
 int countUnsignedNumber(int arrNumber[], int sizeOfArray) {
     int totalUnsignedNumber = 0;
+    for (int i = 0; i < sizeOfArray; ++i) {
+        if(arrNumber[i] > 0){
+            totalUnsignedNumber += arrNumber[i];
+        }
+    }
+    return totalUnsignedNumber;
 }
 
 int main() {
-
+    int sizeOfArray = 3;
+    int arrayNumber[sizeOfArray];
+    arrayNumber[0] = -4;
+    arrayNumber[1] = -14;
+    arrayNumber[2] = 24;
+    printf("Tong cac so nguyen duong la %d", countUnsignedNumber(arrayNumber, sizeOfArray));
 }
