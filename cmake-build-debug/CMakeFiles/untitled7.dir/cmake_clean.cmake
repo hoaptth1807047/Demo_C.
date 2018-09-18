c822c7cf577a6b7806a7b2ce0ef2383988f8befe
Pham Thanh Hoa <hoaptth1807047@fpt.edu.vn>
2018-09-18 17:45:18 +0700

    Khoi tao project

---
+++ cmake-build-debug/CMakeFiles/untitled7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled7.dir/main.c.o"
+  "untitled7.pdb"
+  "untitled7.exe"
+  "libuntitled7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int countUnsignedNumber(int arrNumber[], int sizeOfArray) {
+    int totalUnsignedNumber = 0;
+}
+
+int main() {
+
+}
